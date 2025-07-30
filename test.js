1c8e6444c524b6a830bcff72cab7fe1070df7eef
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-07-30 22:26:20 +0200

    The part 3: Work with the browser events

--- test.js
+++ test.js
@@ -44,3 +44,79 @@ first_element.style.fontSize = "2em";
 
 last_element.style.cssText =
   "color: blue; background-color: orange; width : 30%; font-size: 3.5em";
+
+const clickhere = document.querySelector("#clickhere");
+console.log(clickhere);
+
+clickhere.addEventListener("click", function () {
+  document.body.remove();
+  // console.log("i'm clicked");
+});
+
+//example1.
+const newCustomElement = document.createElement("div");
+
+for (let i = 0; i <= 200; i++) {
+  const newElement = document.createElement("p");
+  newElement.textContent = `This is paragraph number: ${i}`;
+  newElement.addEventListener(
+    "click",
+    function (event) {
+      console.log(event);
+      console.log(`${newElement.innerHTML}`);
+    },
+    { passive: true }
+  );
+  newCustomElement.appendChild(newElement);
+}
+
+// document.body.appendChild(newCustomElement);
+document.body.insertAdjacentElement("beforeend", newCustomElement);
+
+//example 2:
+
+const newCustomDiv = document.createElement("div");
+const respondToTheClickEvent = function (event) {
+  console.log(event);
+  console.log("The element is clicked");
+};
+for (let i = 0; i <= 50; i++) {
+  const newElement = document.createElement("p");
+  newElement.textContent = `This is paragraph number: ${i}`;
+  newElement.addEventListener("click", respondToTheClickEvent, {
+    passive: true,
+  });
+  newCustomDiv.appendChild(newElement);
+}
+document.body.insertAdjacentElement("beforebegin", newCustomDiv);
+
+//example 3:
+
+const newCustomDivbtn = document.createElement("div");
+const respondToTheClickEventbtn = function (event) {
+  const elementedClicked = event.target;
+  elementedClicked.style.cssText =
+    "font-size:2em; background-color:black; color: white; font-weight:bold ";
+  console.log(event);
+  console.log("The element is clicked : " + elementedClicked.innerHTML);
+};
+for (let i = 0; i <= 10; i++) {
+  const newElement = document.createElement("p");
+  newElement.textContent = `This is paragraph number: ${i}`;
+
+  newCustomDivbtn.appendChild(newElement);
+}
+newCustomDivbtn.addEventListener("click", respondToTheClickEventbtn, false);
+coaches.insertAdjacentElement("beforebegin", newCustomDivbtn);
+
+//p and inner span's
+// document.querySelector("#content").addEventListener("click", function (evt) {
+//   console.log("A span was clicked with text " + evt.target.textContent);
+// });
+
+document.querySelector("#content").addEventListener("click", function (evt) {
+  if (evt.target.nodeName === "SPAN") {
+    // ← verifies target is desired element
+    console.log("A span was clicked with text " + evt.target.textContent);
+  }
+});
